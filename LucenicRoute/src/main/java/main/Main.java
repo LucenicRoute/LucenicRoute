96e20d1a0f0b87e32f58ecd9d0220e9671a006e7
arslant-tcd <arslant@tcd.ie>
2021-11-11 12:18:54 +0000

    Updated LATimes Parser Document types

--- LucenicRoute/src/main/java/main/Main.java
+++ LucenicRoute/src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.lucene.document.Document;
@@ -14,31 +15,61 @@ public class Main {
 	public static void main(String[] args) {
 		FBISParser fbis = new FBISParser();
 		FTParser ft = new FTParser();
+		LATimesParser lat = new LATimesParser();
+		FR94Parser fr94 = new FR94Parser();
+		List<Document>  documentList = new ArrayList<Document>();
+		
 		try {
-			List<Document>  documentList = FTParser.parseFT();
-			documentList.addAll(fbis.getFBISDocs());
-			documentList.get(1);
+			System.out.println("Parsing FT...");
+			long startTime = System.nanoTime();
+			documentList.addAll(FTParser.parseFT());
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime-startTime;
+			System.out.print("FT Complete:\tTime taken = " + timeElapsed / 1000000 + "ms\tTotal Corpus Size = " + (documentList.size()) + "\n");
 		} catch (IOException e) {
+			System.out.println("Failed to parse FT");
 			System.out.println(e.getMessage());
 		}
 		
-		LATimesParser lat = new LATimesParser();
-		
 		try {
-			lat.InitializeParsing();
-			System.out.println("The documents were parsed for the Los Angeles Times. Completed...");	
+			System.out.println("Parsing FBIS...");
+			long startTime = System.nanoTime();
+			documentList.addAll(fbis.getFBISDocs());
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime-startTime;
+			System.out.print("FBIS Complete:\tTime taken = " + timeElapsed / 1000000 + "ms\tTotal Corpus Size = " + (documentList.size()) + "\n");
 		} catch (IOException e) {
+			System.out.println("Failed to parse FBIS");
+			System.out.println(e.getMessage());
+		}
+	
+		try {
+			System.out.println("Parsing LAT...");
+			long startTime = System.nanoTime();
+			documentList.addAll(lat.InitializeParsing());
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime-startTime;
+			System.out.println("The documents were parsed for the Los Angeles Times. Completed...");
+			System.out.print("LAT Complete:\tTime taken = " + timeElapsed / 1000000 + "ms\tTotal Corpus Size = " + (documentList.size()) + "\n");
+		} catch (IOException e) {
+			System.out.println("Failed to parse LAT");
+			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 		
-		FR94Parser fr94 = new FR94Parser();
-
 		try {
-			fr94.parseFR("Input/fr94");
-			System.out.println("Completed..");	
+			System.out.println("Parsing FR...");
+			long startTime = System.nanoTime();
+			documentList.addAll(fr94.getFRDocs("Input/fr94"));
+			long endTime = System.nanoTime();
+			long timeElapsed = endTime-startTime;
+			System.out.println(timeElapsed);
+			System.out.print("FR Complete:\tTime taken = " + timeElapsed / 1000000 + "ms\tTotal Corpus Size = " + (documentList.size()) + "\n");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			System.out.println("Failed to parse FR");
 			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 	
 	}
--- LucenicRoute/src/main/java/searchEngine/searchEngine.java
+++ LucenicRoute/src/main/java/searchEngine/searchEngine.java
@@ -1,5 +1,10 @@
 package searchEngine;
 
+
+/**
+ * @author Tolga Arslan
+ */
+
 public class searchEngine {
 	
 
