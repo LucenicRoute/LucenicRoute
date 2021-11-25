00cab0e5237c3e780644b867d06e7a25d48dfe84
crowleos <crowleos@tcd.ie>
2021-11-25 19:15:54 +0000

    Added english possesive filter and basic synonym func

--- LucenicRoute/src/main/java/util/CustomAnalyzer.java
+++ LucenicRoute/src/main/java/util/CustomAnalyzer.java
@@ -10,8 +10,14 @@ import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.LowerCaseFilter;
 import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.en.EnglishPossessiveFilter;
+import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilter;
+import org.apache.lucene.analysis.synonym.SynonymMap;
+import org.apache.lucene.util.CharsRef;
+
 
 public class CustomAnalyzer extends Analyzer{
 
@@ -20,7 +26,24 @@ public class CustomAnalyzer extends Analyzer{
         final StandardTokenizer tokenizer = new StandardTokenizer();
         TokenStream tokenStream = new LowerCaseFilter(tokenizer);
         CharArraySet stopwordSet = processStopwords(Constants.STOPWORDS_FILEPATH);
+
+        tokenStream = new EnglishPossessiveFilter(tokenStream);
+
+        //tokenStream = new SynonymGraphFilter(tokenStream, synonyms, ignoreCase)
+
         tokenStream = new StopFilter(tokenStream, stopwordSet);
+
+        SynonymMap.Builder builder = new SynonymMap.Builder(true);
+        builder.add(new CharsRef("USA"), new CharsRef("United States"), true);
+        builder.add(new CharsRef("USA"), new CharsRef("US"), true);
+        builder.add(new CharsRef("USA"), new CharsRef("United States of America"), true);
+
+        try{
+            tokenStream = new SynonymGraphFilter(tokenStream, builder.build(), true);
+        } catch (IOException e) {
+            System.out.println("ERROR WITH SYNONYMS");
+        }
+
         tokenStream = new SnowballFilter(tokenStream, "English");
         return new TokenStreamComponents(tokenizer, tokenStream);
     }
